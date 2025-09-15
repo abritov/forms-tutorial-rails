5b3f44c554ee1477cc288a330c548cf544e87fcd
fitumi <pkgsail.dev@gmail.com>
2025-09-15 09:40:11 +0300

    add users.create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    logger.debug params
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path(@user.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :email)
   end
 end
