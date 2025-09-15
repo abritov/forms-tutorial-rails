771439d0235aeab19ec7552f69ef81b5a2d978b8
fitumi <pkgsail.dev@gmail.com>
2025-09-15 10:07:19 +0300

    use rails form dsl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to users_path(@user.id)
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_content
     end
   end
 
   private
 
   def user_params
-    params.permit(:username, :password, :email)
+    params.expect(user: [:username, :password, :email])
   end
 end
